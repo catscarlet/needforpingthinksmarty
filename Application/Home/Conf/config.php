4325b3c72e4c273af9f3df3b7de2a7f52fb8e49e
catscarlet <i@catscarlet.com>
2015-08-17 11:49:33 +0800

    update mysql password

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -6,7 +6,7 @@ return array(
 'DB_HOST' => 'localhost',
 'DB_NAME' => 'needforping',
 'DB_USER' => 'thinkphp',
-'DB_PWD' => '5umAQXVrLUsrqW5P',
+'DB_PWD' => 'PEy4cmbnenKWP4db',
 'DB_PORT' => '3306',
 'DB_PREFIX' => 'think_',
 'DB_PARAMS' => array(\PDO::ATTR_CASE => \PDO::CASE_NATURAL), // 数据库连接参数，强制查询返回值大小写
