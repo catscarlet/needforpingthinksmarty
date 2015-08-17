4d2604245ca9089777c87ca812ba15859e965edc
catscarlet <i@catscarlet.com>
2015-08-17 12:27:57 +0800

    update shell . change from reading txt to reading mysql.pinglist.

--- Public/shell/needforping.sh
+++ Public/shell/needforping.sh
@@ -3,9 +3,15 @@
 . needforping.conf
 
 TMP_DIR=/$needforping_DIR/shell/pingresult
-SERVER_LIST=/$needforping_DIR/shell/server_list.txt
+#SERVER_LIST=/$needforping_DIR/shell/server_list.txt
 
-while read line
-    do
-        $needforping_DIR/shell/goping.sh $line &
-    done < $SERVER_LIST
+    i=0
+    MYSQL_COMMAND="SELECT server_name FROM $DB_PREFIX$DB_NAME.pinglist ;"
+    mysql -u$DB_USER -p$DB_PWD -e "$MYSQL_COMMAND" | while read server_name; do
+
+    if [ $i -ne 0 ] ;then
+      $needforping_DIR/shell/goping.sh $server_name &
+      #echo "$server_name"
+    fi
+    i=`expr $i + 1`
+      done
